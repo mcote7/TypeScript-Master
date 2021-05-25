39c1f21d71ab72cf68dbd58500850f7e3d695d5c
Michael Cote <michaelcote7@outlook.com>
2021-05-24 22:28:09 -0700

    .

---
+++ src/app.ts
@@ -0,0 +1,15 @@
+
+
+const myObject = {
+  myMethod() {
+    console.log("object this ::", this)
+  }
+}
+myObject.myMethod();
+
+function myFunction() {
+  console.log("func this ::", this)
+};
+myFunction();
+myFunction.call(myObject);
+myFunction.call([]);
