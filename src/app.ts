78d6dcb06d9c779c67b318fb5511f741df0ed4a4
Michael Cote <michaelcote7@outlook.com>
2021-05-30 16:05:29 -0700

    lololo

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,29 @@
+import * as _ from 'lodash';
 
+function reverse(str: string): string;
+function reverse<T>(arr: T[]): T[];
+
+function reverse<T>(stringOrArray: string | T[]): string | T[] {
+  if(typeof stringOrArray === 'string') {
+    return stringOrArray
+      .split('')
+      .reverse()
+      .join('');
+  }
+  return stringOrArray.slice().reverse();
+}
+
+reverse('pepperoni');
+reverse(['bacon', 'peppers', 'onions', 'sausage']);
+
+//  ENUM types  //
+
+enum Sizes {
+  Sm, // 0
+  Md, // 1
+  Lg // 2
+}
+
+const x = _.map(Sizes); 
+
+console.log(x); // returns array(6) = ['Sm','Md','Lg', 0, 1, 2]; ❔ cool //
