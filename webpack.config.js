e57c4da24033229cf44b944a2e5a7c8b5c92f48e
Michael Cote <michaelcote7@outlook.com>
2022-11-14 12:59:48 -0800

    testing new

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 module.exports = {
-  entry: './src/app.ts',
+  entry: './src/new.ts',
   output: {
-    filename: 'app.js',
+    filename: 'new.js',
     path: __dirname + './dist',
   },
   resolve: {
@@ -14,3 +14,19 @@ module.exports = {
     port: 3000,
   },
 };
+// module.exports = {
+//   entry: './src/app.ts',
+//   output: {
+//     filename: 'app.js',
+//     path: __dirname + './dist',
+//   },
+//   resolve: {
+//     extensions: ['.ts', '.js'],
+//   },
+//   module: {
+//     rules: [{ test: /\.ts$/, use: 'awesome-typescript-loader' }],
+//   },
+//   devServer: {
+//     port: 3000,
+//   },
+// };
